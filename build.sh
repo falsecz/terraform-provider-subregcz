4d480a451bb326f221ba32c6551de4d78ba74df4
Lukas Fuchs <panzelva@gmail.com>
2019-12-18 10:54:20 +0100

    multi-platform build

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' .
+build_package() {
+    # static builds http://blog.wrouesnel.com/articles/Totally%20static%20Go%20builds/
+    CGO_ENABLED=0 GOOS=$1 GOARCH=$2 go build -o terraform-provider-subregcz_$1_$2 -a -ldflags '-extldflags "-static"' .
+}
+
+build_package linux amd64
+build_package darwin amd64
